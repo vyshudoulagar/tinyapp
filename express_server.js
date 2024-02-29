96e292f7ef241ff4e3152cc21d8c7e6e36622725
WishNavy <du.vyshu@gmail.com>
2024-02-29 08:38:31 -0800

    Added a html file for to post new url and used GET and POST methods

--- express_server.js
+++ express_server.js
@@ -9,16 +9,21 @@ const urlDatabase = {
     "9sm5xK": "http://www.google.com"
 };
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/urls', (req, res) => {
     const templateVars = {urls: urlDatabase};
     res.render('urls_index', templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
     const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
     res.render("urls_show", templateVars);
 });
-
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
