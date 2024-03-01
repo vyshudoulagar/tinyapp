c511d6a4369fd199b333ccbffa88a877732026ce
WishNavy <du.vyshu@gmail.com>
2024-03-01 10:51:45 -0800

    added cookie-parser and logout part,added if stmt for header so that login and logout wont be displayed at same time

--- express_server.js
+++ express_server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cookieParser = require('cookie-parser')
 const app = express();
+app.use(cookieParser());
+
 const PORT = 8080; //default port 8080
 
 app.set('view engine', 'ejs');
@@ -12,16 +15,17 @@ const urlDatabase = {
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/urls', (req, res) => {
-    const templateVars = { urls: urlDatabase };
+    const templateVars = { username: req.cookies["username"], urls: urlDatabase };
     res.render('urls_index', templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
+    const templateVars = { username: req.cookies["username"] }
+    res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+    const templateVars = { username: req.cookies["username"], id: req.params.id, longURL: urlDatabase[req.params.id] };
     res.render("urls_show", templateVars);
 });
 
@@ -33,7 +37,7 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/u/:id", (req, res) => {
-    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+    const templateVars = { username: req.cookies["username"], id: req.params.id, longURL: urlDatabase[req.params.id] };
     res.render("urls_show", templateVars);
 });
 
@@ -57,6 +61,13 @@ app.post("/urls/:id", (req, res) => {
 app.post("/login", (req, res) => {
     const username = req.body.username;
     res.cookie('username', username);
+    res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+    console.log('Logging Out. Current Cookies: ', req.cookies);
+    const username = req.body.username;
+    res.clearCookie('username', username);
     res.redirect("/urls");
 });
 
