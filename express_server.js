9d0ffa017c3ce315e3bd069472d47335751ab97d
WishNavy <du.vyshu@gmail.com>
2024-03-03 19:06:11 -0800

    added register template and a get method for it

--- express_server.js
+++ express_server.js
@@ -70,6 +70,10 @@ app.post("/logout", (req, res) => {
     res.redirect("/urls");
 });
 
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
